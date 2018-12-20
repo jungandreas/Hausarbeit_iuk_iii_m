bd8a84d60c632d75968ae4dcb34756f95a215845
jung.andreas <jung.andreas@bluewin.ch>
2018-12-20 21:11:41 +0100

    post and put not implemented

--- public/js/run_sw.js
+++ public/js/run_sw.js
@@ -1,24 +1,11 @@
 // Load serviceWorker
 if ('serviceWorker' in navigator && 'SyncManager' in window) {
-    navigator.serviceWorker.register('./sw.js');
+    navigator.serviceWorker.register('./sw.js')
     console.log('ServiceWorker registration successful with scope: ');
+
 }
 else {
-    document.getElementById('add').addEventListener('click', () => {
-        var payload = {
-            id: data.length,
-            description: document.getElementById('input').value,
-            status: "undone"
-        };
-    fetch(url+'/tasks',
-        {
-            method: 'POST',
-            headers: new Headers({
-                'content-type': 'application/json'
-            }),
-        body: JSON.stringify(payload)
-});
-});
+    console.log('no Service Worker registered');
 }
 
 
